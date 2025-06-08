b4bd2c882715c65a080655c76e57b8730fd32b6e
quqixxx <fofanfrif@gmail.com>
2025-06-09 01:19:50 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -19,17 +19,25 @@ const P_VALUE_HOTELLOOK = '4115';
 const CAMPAIGN_ID_AVIASALES = '100';
 const CAMPAIGN_ID_HOTELLOOK = '101';
 
-// 4. Логирование статуса токена при старте
-console.log(
-    'Статус API токена Travelpayouts (первые 5 символов):', 
-    (TRAVELPAYOUTS_API_TOKEN) 
-        ? TRAVELPAYOUTS_API_TOKEN.substring(0, 5) + '...' 
-        : 'ТОКЕН НЕ УСТАНОВЛЕН!'
-);
+// 4. Настройка CORS для "живого" сайта
+const allowedOrigins = [
+  'http://localhost:5173',                         // Для нашей локальной разработки
+  'https://vputi.netlify.app',                   // Наш основной "красивый" адрес
+  'https://superb-kelpie-fca21d.netlify.app'     // <<< ДОБАВЛЕН НОВЫЙ АДРЕС, С КОТОРОГО БЫЛА ОШИБКА
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
 
 // 5. Подключение middleware
-// ВРЕМЕННО РАЗРЕШАЕМ ЗАПРОСЫ СО ВСЕХ ИСТОЧНИКОВ ДЛЯ ДИАГНОСТИКИ
-app.use(cors()); 
+app.use(cors(corsOptions)); 
 app.use(express.json());
 
 // --- Начало Маршрутов API ---
